0a623c30066a69c769e042ada23b34accc774d1a
definitevictory <159054518+definitevictory@users.noreply.github.com>
2025-03-05 15:07:12 -0700

    a

--- IndexInJavaScriptDOM/scriptB.js
+++ IndexInJavaScriptDOM/scriptB.js
@@ -1,2 +1,6 @@
-Document.getElementById('Header').style.color = 'green'; 
-Document.getElementById('Header').innerHTML = 'Hello World';
+document.getElementsByTagName('body')[0].style.cssText = '	max-width: 100%; height:100%;display: flex; padding: 0;flex-direction:column;flex:1;background-color: rgb(241, 241, 241); margin:0;'
+document.getElementsByTagName('header')[0].style.backgroundColor =  'green';
+document.getElementsByTagName('h1')[0].style.cssText = 'background-color:green; text-align: center;  height: 12vw; font-size: 6vw; color: white; padding-top: 3vw; padding-bottom: 3vw; font-family: Haettenschweiler';
+document.getElementsByTagName('h1')[0].innerHTML = '&#127881; My Second Music Player!!!! &#127881;';
+document.getElementById('introductionbackground').style.cssText = '	background-image: url(https://images.squarespace-cdn.com/content/v1/5e5663314f01390f23459410/06ce5dbb-458f-4cc3-85fb-c15fb50c910d/Gordon+Ramsay+using+Scanpan+on+masterchef.jpeg); background-size: cover; background-repeat: no-repeat; background-attachment: scroll; background-position: center; height: 50vw;';
+document.getElementById('IntroductionSection').style.cssText = '	padding: 3vw;width:55%;height:91%;float:right;margin-top:0;margin-right: 0;font-size: 2vw;border-color:rgb(90, 101, 255);border-style: solid;border-bottom-left-radius:25vw ; background-color: #7be2ff;';
