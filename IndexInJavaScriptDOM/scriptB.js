1a0088eeade171151640755c10e675183f6a5eb1
definitevictory <159054518+definitevictory@users.noreply.github.com>
2025-03-06 14:12:32 -0700

    a

--- IndexInJavaScriptDOM/scriptB.js
+++ IndexInJavaScriptDOM/scriptB.js
@@ -4,3 +4,8 @@ document.getElementsByTagName('h1')[0].style.cssText = 'background-color:green; 
 document.getElementsByTagName('h1')[0].innerHTML = '&#127881; My Second Music Player!!!! &#127881;';
 document.getElementById('introductionbackground').style.cssText = '	background-image: url(https://images.squarespace-cdn.com/content/v1/5e5663314f01390f23459410/06ce5dbb-458f-4cc3-85fb-c15fb50c910d/Gordon+Ramsay+using+Scanpan+on+masterchef.jpeg); background-size: cover; background-repeat: no-repeat; background-attachment: scroll; background-position: center; height: 50vw;';
 document.getElementById('IntroductionSection').style.cssText = '	padding: 3vw;width:55%;height:91%;float:right;margin-top:0;margin-right: 0;font-size: 2vw;border-color:rgb(90, 101, 255);border-style: solid;border-bottom-left-radius:25vw ; background-color: #7be2ff;';
+document.getElementById('IntroductionSection').childNodes[0].style.cssText = 'border-right-color:#00000076; border-right-style:solid; border-right-width:0.1vw; padding-right: 2vw;';
+document.getElementById('IntroductionSection').childNodes[0].childNodes[0].innerHTML ='Name? : Door Key';
+
+document.getElementById('IntroductionSection').childNodes[1].childNodes[0].innerHTML ='My Interests Are:';
+
